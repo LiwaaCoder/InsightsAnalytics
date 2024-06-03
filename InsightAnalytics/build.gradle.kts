b8304e4854b60f43de4144c0675e62b2c0bdccb9
LiwaaHosh <66652532+LiwaaCoder@users.noreply.github.com>
2024-06-03 19:32:44 +0300

    ready to publish 1

--- InsightAnalytics/build.gradle.kts
+++ InsightAnalytics/build.gradle.kts
@@ -40,25 +40,20 @@ buildscript {
     }
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("release_new1") {
-        }
-    }
-}
-
-
 
 
 dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
     implementation("com.google.firebase:firebase-analytics")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation ("com.google.firebase:firebase-firestore:25.0.0")
     implementation ("com.google.firebase:firebase-core:21.1.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("com.github.LiwaaCoder:InsightsAnalytics:2.1.1")
+
 }
